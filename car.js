042cf1c55e4e948cd5e5ed0fff531dfe72236ec0
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2024-01-04 00:47:46 +0100

    feat: car motion assimilation completed

--- car.js
+++ car.js
@@ -6,8 +6,9 @@ class Car{
         this.height = height
         this.speed = 0
         this.maxSpeed = 3
-        this.acceleration = 0.2
+        this.acceleration = 0.3
         this.friction = 0.05
+        this.angle = 0
 
         this.controls = new Controls()
     }
@@ -37,20 +38,39 @@ class Car{
             this.speed += this.friction
         }
 
-        this.y -= this.speed
+        if (Math.abs(this.speed) < this.friction){
+            this.speed = 0
+        }
+
+        if(this.speed != 0){
+            const flip = this.speed > 0 ? 1: -1
+            if (this.controls.left) {
+                this.angle -= 0.03 * flip
+            }
+
+            if (this.controls.right) {
+                this.angle += 0.03 * flip
+            }
+        }
+
+        this.x -= Math.sin(-this.angle) * this.speed 
+        this.y -= Math.cos(-this.angle) * this.speed
+
     }
 
     draw(ctx){
+        ctx.save()
         ctx.beginPath()
-
+        ctx.translate(this.x, this.y)
+        ctx.rotate(this.angle)
         ctx.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
+            - this.width / 2,
+            - this.height / 2,
             this.width,
             this.height
         )
 
         ctx.fill()
-
+        ctx.restore()
     }
 }   
