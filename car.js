dca7c182048d15af4cd336fc71fdba36de6ba4e0
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2024-01-07 15:23:25 +0100

    fix: refactored update method of the class Car

--- car.js
+++ car.js
@@ -14,6 +14,10 @@ class Car {
     }
 
     update () {
+       this.#move()
+    }
+
+    #move(){
         if (this.controls.forward) this.speed += this.acceleration
 
         if (this.controls.backward) this.speed -= this.acceleration
@@ -38,7 +42,6 @@ class Car {
         
         this.x += Math.sin(this.angle) * this.speed
         this.y -= Math.cos(this.angle) * this.speed
-       
     }
 
     draw(ctx) {
