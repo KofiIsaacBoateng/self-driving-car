04084157328dc641c00794d82f118e3d7a36ad17
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2024-01-07 16:46:14 +0100

    update: Road movement illusion

--- main.js
+++ main.js
@@ -8,10 +8,17 @@ const road = new Road(canvas.width / 2, canvas.width * 0.9)
 
 
 const animate = () => {
+    ctx.save()
+
     car.update()
     canvas.height = window.innerHeight
+
+    ctx.translate(0, - car.y + canvas.height * 0.7 )
+
     road.draw(ctx)
     car.draw(ctx)
+
+    ctx.restore()
 
     requestAnimationFrame(animate)
 }
